f0596614b6352fcb48d69f5e258da5f8eae38439
Athul Rajagopal <49791750+rajagoat@users.noreply.github.com>
2023-02-23 19:22:36 -0700

    navbar styled

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,39 @@
+import Image from "next/image";
+import Link from "next/link";
+import styles from "../styles/Navbar.module.css"
+
 const Navbar = () => {
     return (
-        <nav>
-            <h1>Navbar goes here</h1>
+        <nav className={styles.navbar}>
+            <div className={styles.logo}>
+                <Link href="/">
+                    <Image
+                        src="/icons/favicon.png"
+                        width={74}
+                        height={74}
+                        alt="main logo, click to go back to homepage"
+                    />
+                </Link>
+            </div>
+            <div className={styles.links}>
+                <Link href="/experience">
+                    Experience
+                </Link>
+                <Link href="/projects">
+                    Projects
+                </Link>
+                <Link href="/about">
+                    About
+                </Link>
+                <Image
+                    src="/icons/light-mode.svg"
+                    width={49.5}
+                    height={49.5}
+                    alt="sun icon, click to switch to dark mode"
+                />
+            </div>
         </nav>
     );
 }
- 
+
 export default Navbar;
--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
       <Head>
         <title>Home | Athul Rajagopal</title>
         <meta name="description" content="Portfolio and personal website of Athul Rajagopal - Software Engineering and Commerce student at the University of Calgary." />
-        <link rel="icon" href="/icons/favicon.ico" />
+        <link rel="icon" type="image/png" href="/icons/favicon.png" />
       </Head>
 
       <h1>Homepage</h1>
