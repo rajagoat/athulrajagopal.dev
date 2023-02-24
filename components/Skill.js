aaab8b76f94de4b1fe175349f8bee0f01fe90747
Athul Rajagopal <49791750+rajagoat@users.noreply.github.com>
2023-02-23 20:58:09 -0700

    switched to next/font

--- components/Skill.js
+++ components/Skill.js
@@ -1,8 +1,15 @@
+import { Lora } from "next/font/google";
+
+const lora = Lora({
+  subsets: ['latin'],
+  weight: ['400']
+});
+
 const Skill = () => {
     return (
-        <>
+        <div className={lora.className}>
         
-        </>
+        </div>
     );
 }
  
