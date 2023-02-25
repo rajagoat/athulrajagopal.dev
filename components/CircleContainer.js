2d696ac194277d77d7d8359ca2d420c344f5a2bb
Athul Rajagopal <49791750+rajagoat@users.noreply.github.com>
2023-02-25 00:28:41 -0700

    moved circle logic to separate component

---
+++ components/CircleContainer.js
@@ -0,0 +1,28 @@
+import Image from 'next/image'
+import Circle from '../components/Circle'
+import styles from '../styles/CircleContainer.module.css'
+import useViewport from "../hooks/useViewport";
+
+const CircleContainer = () => {
+    const { width } = useViewport();
+    const BREAKPOINT = 762;
+
+    return (
+        <div className={styles.circleContainer}>
+            <Circle size={89} backgroundColor={"#0559DB"} minTop={-10} top={72.8} minLeft={-21} left={69} />
+            <Circle size={208} backgroundColor={"#4088EC"} minTop={-102} top={-72} minLeft={240} left={370} />
+            <Circle size={220} backgroundColor={"#C0DFFC"} minTop={116} top={271} minLeft={6} left={111.5} />
+            <Circle size={78} backgroundColor={"#7DB5F6"} minTop={284} top={539.5} minLeft={227} left={459} />
+            <Image
+                src="/assets/memoji-wave.jpg"
+                width={width < BREAKPOINT ? 220 : 346.27}
+                height={width < BREAKPOINT ? 256 : 402.93}
+                className={styles.memoji}
+                alt="Apple Memoji waving"
+                priority
+            />
+        </div>
+    );
+}
+
+export default CircleContainer;
