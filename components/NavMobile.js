b8ae02d189a786ca17726d54a8b6eb7472729350
Athul Rajagopal <49791750+rajagoat@users.noreply.github.com>
2023-02-24 17:20:34 -0700

    phone navbar fully functional

--- components/NavMobile.js
+++ components/NavMobile.js
@@ -2,37 +2,96 @@ import Image from "next/image";
 import Link from "next/link";
 import styles from "../styles/NavMobile.module.css"
 import { Icon } from "@iconify/react";
+import themeIcon from '@iconify/icons-mdi/theme-light-dark';
 import menuIcon from '@iconify/icons-mdi/menu';
 import closeIcon from '@iconify/icons-mdi/close';
 import useTheme from "../hooks/useTheme";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 
 const NavMobile = () => {
+    const router = useRouter();
     const { activeTheme, setActiveTheme } = useTheme();
+    const [menuIsOpen, setMenuIsOpen] = useState(false);
 
-    const openNav = () => {
-        console.log('navbar opened');
+    const toggleTheme = () => {
+        const newTheme = activeTheme === 'light' ? 'dark' : 'light';
+        setActiveTheme(newTheme);
+    };
+
+    const openMenu = () => {
+        setMenuIsOpen(true);
     }
 
+    const closeMenu = () => {
+        setMenuIsOpen(false);
+    }
+
+    // this will close the menu on each redirect
+    useEffect(() => {
+        setMenuIsOpen(false);
+    }, [router]);
+
     return (
-        <nav className={`${styles.navbar} ${activeTheme === "dark" ? styles.navbarDark : styles.navbarLight}`}>
-            <div className={styles.logo}>
-                <Link href="/">
-                    <Image
-                        src="/icons/favicon.png"
-                        width={31}
-                        height={31}
-                        alt="main logo, click to go back to homepage"
-                    />
-                </Link>
-            </div>
-            <Icon
+        <>
+            <nav className={`${styles.navbar} ${activeTheme === "dark" ? styles.navbarDark : styles.navbarLight}`}>
+                <div className={styles.logo}>
+                    <Link href="/">
+                        <Image
+                            src="/icons/favicon.png"
+                            width={31}
+                            height={31}
+                            alt="main logo, click to go back to homepage"
+                        />
+                    </Link>
+                </div>
+                <Icon
                     icon={menuIcon}
                     width="31"
                     height="31"
-                    onClick={() => openNav()}
+                    onClick={() => openMenu()}
                     style={{ cursor: "pointer" }}
-            />
-        </nav>
+                />
+            </nav>
+            {menuIsOpen && <div className={styles.menu}>
+                <Icon
+                    icon={closeIcon}
+                    width="31"
+                    height="31"
+                    onClick={() => closeMenu()}
+                    style={{ cursor: "pointer" }}
+                />
+                <div className={styles.links}>
+                    <Link href="/experience">
+                        Experience
+                    </Link>
+                    <Link href="/projects">
+                        Projects
+                    </Link>
+                    <div className={styles.subLinks}>
+                        <Link href="/projects/gleanr">
+                            Gleanr
+                        </Link>
+                        <Link href="/projects/blue-nile-sport">
+                            Blue Nile Sport
+                        </Link>
+                        <Link href="/projects/onthehouse">
+                            OnTheHouse
+                        </Link>
+                    </div>
+                    <Link href="/about">
+                        About
+                    </Link>
+                    <Icon
+                        icon={themeIcon}
+                        width="31"
+                        height="31"
+                        onClick={() => toggleTheme()}
+                        style={{ cursor: "pointer" }}
+                    />
+                </div>
+            </div>}
+        </>
     );
 }
 
