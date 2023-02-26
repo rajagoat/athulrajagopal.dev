bee555b59b60f924626a6868e84ce6b323bad906
Athul Rajagopal <49791750+rajagoat@users.noreply.github.com>
2023-02-25 23:26:45 -0700

    waves look good on mobile fr this time

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image';
-import useTheme from "../hooks/useTheme";
 import styles from '../styles/Home.module.css'
 import CircleContainer from '../components/CircleContainer';
 import { Icon } from '@iconify/react';
 import arrowDown from '@iconify/icons-mdi/arrow-down';
-import layeredWavesDark from "../public/assets/layered-waves-haikei-dark.svg";
-import layeredWavesLight from "../public/assets/layered-waves-haikei-light.svg";
+import undrawTeam from "../public/assets/undraw-engineering-team.svg";
 
 export default function Home() {
-  const { activeTheme } = useTheme();
-
   return (
     <div className={styles.container}>
       <Head>
@@ -29,14 +25,31 @@ export default function Home() {
           className={styles.arrowDown}
         />
       </section>
-      <div className={styles.divider}>
-        <Image
-          src={activeTheme === "dark" ? layeredWavesDark : layeredWavesLight}
+      <div className={styles.offset}>
+        <div className={styles.divider}>
+          {/* <Image
+          src={layeredWaves}
           fill
           alt="waves in several shades of blue"
-        />
+        /> */}
+        </div>
+        <section className={styles.main}>
+          <div className={styles.mainText}>
+            <p>Welcome to my page!</p>
+            <p>I’m currently an undergraduate student at the <b>University of Calgary</b>, pursuing a dual major in <b>Software Engineering</b> and <b>Commerce</b>.</p>
+            <p>With more than two years of experience developing software for non-profit organizations, student teams, and hackathons, I am well-versed in creating quality software solutions to address real-world problems.</p>
+          </div>
+          <Image
+            src={undrawTeam}
+            width={280}
+            height={143}
+            alt="team of software engineers speaking with one another"
+          />
+          <div className={styles.mainText}>
+            <p>Here’s some of the tools and technologies I have picked up along the way:</p>
+          </div>
+        </section>
       </div>
-
     </div>
   )
 }
