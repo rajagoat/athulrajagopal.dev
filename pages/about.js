1c1d29f33cf047035390913532c2d4c671a8ee0d
Athul Rajagopal <49791750+rajagoat@users.noreply.github.com>
2023-03-05 13:17:20 -0700

    aligned about section header

--- pages/about.js
+++ pages/about.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import styles from "../styles/About.module.css";
 
 const About = () => {
     return (
@@ -8,7 +9,9 @@ const About = () => {
                 <meta name="description" content="Portfolio and personal website of Athul Rajagopal - Software Engineering and Commerce student at the University of Calgary." />
                 <link rel="icon" type="image/png" href="/icons/favicon.png" />
             </Head>
-            <h1>About</h1>
+            <main className={styles.about}>
+                <h1>About</h1>
+            </main>
         </>
     );
 }
