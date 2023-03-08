84175a1c1dd8c759a67a2f3eab66218a020677c9
Athul Rajagopal <49791750+rajagoat@users.noreply.github.com>
2023-03-07 18:26:08 -0700

    moved og info to _document.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import dynamic from "next/dynamic";
-import Head from "next/head";
 import Layout from '../components/Layout'
 import { MenuContextProvider } from "../context/MenuContext";
 import '../styles/globals.css'
@@ -18,24 +17,6 @@ function MyApp({ Component, pageProps }) {
       <ViewportContextProvider>
         <MenuContextProvider>
           <Layout>
-            <Head>
-              <link rel="icon" type="image/png" href="/icons/favicon.png" />
-              <meta name="description" content="Portfolio and personal website of Athul Rajagopal - Software Engineering and Commerce student at the University of Calgary." />
-              <meta name="author" content="Athul Rajagopal" />
-              {/* Facebook Meta Tags */}
-              <meta property="og:url" content="https://www.athulrajagopal.dev/" />
-              <meta property="og:type" content="website" />
-              <meta name="title" property="og:title" content="Home | Athul Rajagopal" />
-              <meta name="description" property="og:description" content="Portfolio and personal website of Athul Rajagopal - Software Engineering and Commerce student at the University of Calgary." />
-              <meta name="image" property="og:image" content="https://www.athulrajagopal.dev/_next/image?url=/assets/og-preview.png&w=1200&q=75" />
-              {/* Twitter Meta Tags */}
-              <meta name="twitter:card" content="summary_large_image" />
-              <meta property="twitter:domain" content="athulrajagopal.dev" />
-              <meta property="twitter:url" content="https://www.athulrajagopal.dev/" />
-              <meta name="twitter:title" content="Home | Athul Rajagopal" />
-              <meta name="twitter:description" content="Portfolio and personal website of Athul Rajagopal - Software Engineering and Commerce student at the University of Calgary." />
-              <meta name="twitter:image" content="https://www.athulrajagopal.dev/_next/image?url=/assets/og-preview.png&w=1200&q=75" />
-            </Head>
             <Component {...pageProps} />
           </Layout >
         </MenuContextProvider>
--- pages/_document.js
+++ pages/_document.js
@@ -20,8 +20,25 @@ class MyDocument extends Document {
         `;
         return (
             <Html>
-                <Head />
-                <body style={{overflowX: "hidden"}}>
+                <Head>
+                    <link rel="icon" type="image/png" href="/icons/favicon.png" />
+                    <meta name="description" content="Portfolio and personal website of Athul Rajagopal - Software Engineering and Commerce student at the University of Calgary." />
+                    <meta name="author" content="Athul Rajagopal" />
+                    {/* Facebook Meta Tags */}
+                    <meta property="og:url" content="https://www.athulrajagopal.dev/" />
+                    <meta property="og:type" content="website" />
+                    <meta property="og:title" content="Home | Athul Rajagopal" />
+                    <meta property="og:description" content="Portfolio and personal website of Athul Rajagopal - Software Engineering and Commerce student at the University of Calgary." />
+                    <meta property="og:image" content="https://www.athulrajagopal.dev/_next/image?url=/assets/og-preview.png&w=1200&q=75" />
+                    {/* Twitter Meta Tags */}
+                    <meta name="twitter:card" content="summary_large_image" />
+                    <meta property="twitter:domain" content="athulrajagopal.dev" />
+                    <meta property="twitter:url" content="https://www.athulrajagopal.dev/" />
+                    <meta name="twitter:title" content="Home | Athul Rajagopal" />
+                    <meta name="twitter:description" content="Portfolio and personal website of Athul Rajagopal - Software Engineering and Commerce student at the University of Calgary." />
+                    <meta name="twitter:image" content="https://www.athulrajagopal.dev/_next/image?url=/assets/og-preview.png&w=1200&q=75" />
+                </Head>
+                <body style={{ overflowX: "hidden" }}>
                     <script dangerouslySetInnerHTML={{ __html: setInitialTheme }} />
                     <Main />
                     <NextScript />
